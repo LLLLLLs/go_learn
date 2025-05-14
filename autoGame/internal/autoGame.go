c6c669574115e08291efb70b504f99c9934cc032
LLLLLLs <1181947970@qq.com>
2025-05-14 22:51:31 +0800

    技能补完

--- autoGame/internal/autoGame.go
+++ autoGame/internal/autoGame.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"autogame/pkg/util"
 	"fmt"
 	"gocv.io/x/gocv"
 	"image"
@@ -31,6 +30,9 @@ func tick() {
 	if clickCheck(img) {
 		return
 	}
+	if upgradeCheck(img) {
+		return
+	}
 }
 
 func clickCheck(img gocv.Mat) bool {
@@ -47,13 +49,16 @@ func clickCheckOnce(img, tmpl gocv.Mat) bool {
 	if !matched {
 		return false
 	}
-	center := image.Point{
-		X: maxLoc.X + tmpl.Cols()/2 + 40,
-		Y: maxLoc.Y + tmpl.Rows()/2 + 150,
-	}
-	fmt.Printf("找到目标，点击中心: %+v\n", center)
-	util.Click(center.X, center.Y)
+	clickPoint(maxLoc, tmpl)
 	return true
+}
+
+func clickPoint(start image.Point, tmpl gocv.Mat) {
+	center := image.Point{
+		X: start.X + tmpl.Cols()/2 + 40,
+		Y: start.Y + tmpl.Rows()/2 + 150,
+	}
+	Click(center.X, center.Y)
 }
 
 func matchResult(img, tmpl gocv.Mat) (maxLoc image.Point, matched bool) {
@@ -63,12 +68,37 @@ func matchResult(img, tmpl gocv.Mat) (maxLoc image.Point, matched bool) {
 
 	var maxVal float32
 	_, maxVal, _, maxLoc = gocv.MinMaxLoc(result)
-	matched = maxVal > 0.9
+	matched = maxVal > 0.95
 	return
 }
 
-func checkUpgrade(img gocv.Mat) bool {
+func upgradeCheck(img gocv.Mat) bool {
 	if _, matched := matchResult(img, upgradeTmpl); !matched {
 		return false
 	}
+	canRefresh = nil
+	for i := range skillTmpl {
+		if i > len(skillTmpl)*2/3 && refresh(img) {
+			return true
+		}
+		maxLoc, matched := matchResult(img, skillTmpl[i])
+		if matched {
+			clickPoint(maxLoc, skillTmpl[i])
+		}
+	}
+	return true
+}
+
+var refreshLoc image.Point
+var canRefresh *bool
+
+func refresh(img gocv.Mat) bool {
+	if canRefresh == nil {
+		refreshLoc, *canRefresh = matchResult(img, refreshTmpl)
+	}
+	if !(*canRefresh) {
+		return false
+	}
+	clickPoint(refreshLoc, refreshTmpl)
+	return true
 }
